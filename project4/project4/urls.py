768dbffce67d0d9133506ff41eb0df3ef82f382b
Oskar Johansson <92440552+OskarJ1993@users.noreply.github.com>
2023-05-15 14:42:59 +0200

    add register/login model and views

--- project4/project4/urls.py
+++ project4/project4/urls.py
@@ -1,21 +1,28 @@
-"""project4 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from products import views
+from register.register_views import register_view
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_view
+from register.register_views import login_view
+from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", views.index, name="index"),
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('phones/', views.phones, name="phones"),
+    path('accessories/', views.accessories, name="accessories"),
+    path('search/', views.search, name='search'),
+    path('cart/', views.show_cart, name='cart'),
+    path('add_to_cart/', views.add_to_cart, name='add_to_cart'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
+    path('cart/clear/', views.clear_cart, name='clear_cart'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
