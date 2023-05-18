174baa17e70e3e36add98ed5e23b2cacbe1d9f15
Oskar Johansson <92440552+OskarJ1993@users.noreply.github.com>
2023-05-18 21:15:30 +0200

    add whitenoise for deployment

--- project4/project4/settings.py
+++ project4/project4/settings.py
@@ -9,7 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7mu46wg3^(2c23kq0lp5s=u8(h+j(&7z=wu-!_+dlf%_58-6&u'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '7mu46wg3^(2c23kq0lp5s=u8(h+j(&7z=wu-!_+dlf%_58-6&u')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
