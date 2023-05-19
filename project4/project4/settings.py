3554167d4de88094728ff77913baf45014ffb6b5
Oskar Johansson <92440552+OskarJ1993@users.noreply.github.com>
2023-05-19 10:24:35 +0200

    adding more content

--- project4/project4/settings.py
+++ project4/project4/settings.py
@@ -131,8 +131,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [
